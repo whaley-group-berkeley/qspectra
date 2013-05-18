9a9fd641e878a94368e91fbe465787d58d0ddb76
Stephan Hoyer <shoyer@berkeley.edu>
2013-05-18 16:56:25 -0700

    Fixed Boltzmann's constant
    Also added all relevant digits to the speed of light
    (used for fs to cm-1 conversion)

--- constants.py
+++ constants.py
@@ -5,12 +5,13 @@ units.
 from numpy import pi, sqrt, log
 
 # multiply by this constant to convert from fs to linear cm^-1:
-CM_FS_LINEAR = 2.99792e-5
+CM_FS_LINEAR = 2.99792458e-5
 # multiply by this constant to convert from fs to angular cm^-1:
 CM_FS = pi * 2 * CM_FS_LINEAR
+
 # multiply by this constant to convert from degrees K to angular cm^-1,
-# by implicitly inserting Boltzmann's constant):
-CM_K = 0.6954
+# by implicitly inserting Boltzmann's constant:
+CM_K = 0.69503476
 
 # ratio between standard deviation and full-width-at-half-maximum for a
 # gaussian function:
