bdf4eb67a145d2284f0eb3eb15af5aa88506079b
Stephan Hoyer <shoyer@gmail.com>
2013-09-22 14:02:30 -0700

    Added ZOFESpaceOperator and cleaned up the DynamicalModel hierarchy
    This should provide a template for how to add restricted subspaces to
    ZOFE dynamics.

--- qspectra/dynamics/zofe.py
+++ qspectra/dynamics/zofe.py
@@ -1,58 +1,59 @@
 import numpy as np
 
-from .liouville_space import LiouvilleSpaceModel
+from ..bath import PseudomodeBath
 from ..utils import memoized_property
+
+from .generic import DynamicalModel, SystemOperator
 from .zofe_rhodot_oopdot import rhodot_oopdot_vec
 from .vectorize_devectorize import vec
-from ..bath import PseudomodeBath
-
-# def ee_index_map(N):
-#     """
-#     Builds matrix M so that M[n,m]=i, where n and m are the labels of the two excited monomers
-#     in the double-exciton states and i is the basis index of the corresponding double-exciton state.
-#     Needs: 
-#     import numpy as np
-#     JR20111111
-#     """
-#     M = np.zeros((N,N),int)
-#     i = N
-#     for n in range(N):
-#         for m in range(n + 1, N):
-#             i += 1
-#             M[n,m] = i
-#     return M
 
 
-# def coupling_operator_L_n(numb_monomers, BS, with_ground_state=False, two_excitons=False):
-#     """
-#     Builds the system operator L_n for the system-bath coupling that is used
-#     in the ZOFE master equation.
-#     Needs: 
-#     import numpy as np
-#     ee_index_map
-#     JR20130227.
-#     """
-#     # If Stephan's system_bath_couplings(subspace) method of the hamiltonia class does the same as this function, we can drop this function. This has to be checked.
-    
-#     ee_ind = ee_index_map(numb_monomers)
-#     L_n = np.zeros((numb_monomers,BS,BS), dtype=complex)
-#     for n in np.arange(numb_monomers):
-#         if not with_ground_state:
-#             L_n[n,n,n] -= 1.  # single exciton part WITHOUT ground state, i.e. numb_monomers = BS
-#         if with_ground_state:
-#             L_n[n,n+1,n+1] -= 1.  # single exciton part WITH ground state, i.e. numb_monomers = BS-1
-#             if two_excitons:
-#                 for m in np.arange(numb_monomers):
-#                     i = ee_ind[n,m]
-#                     if not i==0: L_n[n,i,i] -= 2. # two exciton part WITH ground state
-#     return L_n
+class ZOFESpaceOperator(SystemOperator):
+    def __init__(self, operator, liouv_subspace_map, dynamical_model):
+        """
+        Parameters
+        ----------
+        operator : np.ndarray
+            Matrix representation of the operator in the Hilbert subspace of
+            `dynamical_model`.
+        liouv_subspace_map : string
+            String in the form 'eg->ee' indicating the mapping between
+            Liouville subspaces on which the operator should act.
+        dynamical_model : ZOFEModel
+            ZOFE dynamical model on which this operator acts.
+        """
+        from_space, to_space = liouv_subspace_map.split('->')
+        L1, R1 = map(dynamical_model.hilbert_subspace_indices, from_space)
+        L2, R2 = map(dynamical_model.hilbert_subspace_indices, to_space)
+        self.operator_L = operator[L2, L1]
+        self.operator_R = operator[R1, R2]
+
+    def left_multiply(self, state):
+        rho0, oop0 = self.dynamical_model.state_vec_to_operators(state)
+        rho1 = self.operator_L.dot(rho0)
+        oop1 = np.einsum('cd,abde->abcd', self.operator_L, oop0)
+        return self.operators_to_state_vec(rho1, oop1)
+
+    def right_multiply(self, state):
+        rho0, oop0 = self.dynamical_model.state_vec_to_operators(state)
+        rho1 = rho0.dot(self.operator_R)
+        oop1 = oop0.dot(self.operator_R)
+        return self.operators_to_state_vec(rho1, oop1)
+
+    @memoized_property
+    def expectation_value(self, state):
+        rho0, _ = self.dynamical_model.state_vec_to_operators(state)
+        # Proof: tr M rho = \sum_{ij} M_ij rho_ji
+        return np.einsum('ij,ji', self.operator_L, rho0)
 
 
-class ZOFEModel(LiouvilleSpaceModel):
+class ZOFEModel(DynamicalModel):
+    SystemOperator = ZOFESpaceOperator
+
     def __init__(self, hamiltonian, rw_freq=None, hilbert_subspace='gef',
                  unit_convert=1):
         """
-        DynamicalModel for ZOFE master equation 
+        DynamicalModel for ZOFE master equation
 
         Assumes that each pigment is coupled to an identical, independent bath
 
@@ -66,20 +67,21 @@ class ZOFEModel(LiouvilleSpaceModel):
             of the Hamiltonian.
         hilbert_subspace : container, default 'ge'
             Container of any or all of 'g', 'e' and 'f' indicating the desired
-            Hilbert subspace 
+            Hilbert subspace
         unit_convert : number, optional
             Unit conversion from energy to time units (default 1).
-        
+
         References
         ----------
         See references in module containing the ZOFE master equation
         """
-        super(ZOFEModel, self).__init__(hamiltonian, rw_freq,
-                                            hilbert_subspace, unit_convert)
+        super(ZOFEModel, self).__init__(
+             hamiltonian, rw_freq, hilbert_subspace, unit_convert)
 
         # initial auxiliary operator for the ZOFE master equation
         if not isinstance(self.hamiltonian.bath, PseudomodeBath):
-            raise NotImplementedError
+            raise NotImplementedError('ZOFE only implemented for baths of type '
+                                      'PseudomodeBath')
 
         numb_pm = self.hamiltonian.bath.numb_pm
         n_sit = self.hamiltonian.n_sites
@@ -88,7 +90,7 @@ class ZOFEModel(LiouvilleSpaceModel):
         self.initial_oop = np.zeros((numb_pm, n_sit, n_stat, n_stat), dtype=complex)
         self.initial_oop_vec = vec(self.initial_oop)
 
-        
+
     def equation_of_motion(self, liouville_subspace, heisenberg_picture=False):
         """
         Return the equation of motion for this dynamical model in the given
@@ -97,10 +99,9 @@ class ZOFEModel(LiouvilleSpaceModel):
         """
 
         if heisenberg_picture:
-            raise NotImplementedError
+            raise NotImplementedError('ZOFE not implemented in the Heisenberg '
+                                      'picture')
 
-        
-                
         #L_n = coupling_operator_L_n(n_sites, n_states, with_ground_state=False, two_excitons=False)
         # This is the function to build the coupling operators L_n that I had previously used for ZOFE calculations and checked in the
         # cases of 'ge' and 'e' subspaces. However, in the two-exciton case 'gef', I have never checked if ZOFE gives the
@@ -109,7 +110,7 @@ class ZOFEModel(LiouvilleSpaceModel):
         L_n = [-1.*L for L in self.hamiltonian.system_bath_couplings(subspace=self.hilbert_subspace)]
         # NOTE THE MINUS SIGN!!
         # For the subspace cases 'ge' and 'e', the system_bath_couplings() method and the function coupling_operator_L_n()
-        # that is commented out above give the same coupling operators. 
+        # that is commented out above give the same coupling operators.
         # BUT, for the two-exciton case 'gef', they give different results!
         # It is not clear yet, which of the two the right version for ZOFE is.
         # I have to go through my notes about the extension of ZOFE to the two-exciton space again.
@@ -119,7 +120,7 @@ class ZOFEModel(LiouvilleSpaceModel):
         Omega = self.hamiltonian.bath.Omega
         gamma = self.hamiltonian.bath.gamma
         huang = self.hamiltonian.bath.huang
-        
+
         Gamma = Omega**2*huang
         w = 1j*Omega+gamma
 
@@ -130,10 +131,3 @@ class ZOFEModel(LiouvilleSpaceModel):
         def eom(t, rho_oop_vec):
             return self.unit_convert * rhodot_oopdot_vec(t, rho_oop_vec, oop_shape, sys_ham, L_n, Gamma, w)
         return eom
-
-    # this is just a dummy. Should probably do this more elegantly somehow, without this dummy.
-    @memoized_property
-    def evolution_super_operator(self):
-        return "nothing"
-
-    
