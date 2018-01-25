7e692b46bd8b5bb46f623e394b651689d6bf227f
Donghyun <donghyun208@gmail.com>
2018-01-25 00:05:41 -0800

    clear all notebook outputs

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='qspectra',
     description='QSpectra: Nonlinear Spectroscopy for Molecular Aggregates',
     version='0.1-dev',
-    url='http://github.com/shoyer/qspectra',
+    url='http://github.com/whaley-group-berkeley/qspectra',
     packages=['qspectra',]
 )
