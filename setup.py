fc64fe8fcf6bb908d516a307d155891fe78345d0
Stephan Hoyer <shoyer@gmail.com>
2021-11-12 18:32:39 -0800

    Switch to using setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+import setuptools
 
 setup(
     name='qspectra',
     description='QSpectra: Nonlinear Spectroscopy for Molecular Aggregates',
     version='0.1-dev',
     url='http://github.com/whaley-group-berkeley/qspectra',
-    packages=['qspectra',]
+    packages=setuptools.find_packages(),
 )
